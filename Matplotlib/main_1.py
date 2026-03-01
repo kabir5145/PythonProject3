8fac6ce918551978a3430fca768ddbc148d94425
Kabir5145 <kabir10112006@gmail.com>
2026-03-01 13:05:34 +0530

    Matplotlib intro

---
+++ Matplotlib/main_1.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+# print(matplotlib.__version__)
+
+X = np.array([2023,2024,2025,2026])
+Y = np.array([15,25,30,20])
+
+plt.plot(X,Y)
+# plt.plot(Y)
+# plt.plot(X)
+plt.show()
