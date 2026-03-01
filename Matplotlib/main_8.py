6493d074e8eb0f24fe7fd0cc10ef455ed8ff7ead
Kabir5145 <kabir10112006@gmail.com>
2026-03-01 19:40:52 +0530

    Histograms

---
+++ Matplotlib/main_8.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Histogram = A visual representation of the distribution of quantitative data.
+#             They group values into bins (intervals)
+#             and counts how many fall in each range.
+
+scores = np.random.normal(loc = 80,scale = 10,size = 100)
+scores = np.clip(scores,0,100)
+
+plt.hist(scores,bins = 10,color = "red",edgecolor = "black")
+
+plt.title("Exam scores")
+plt.xlabel("Score")
+plt.ylabel("Frequency")
+plt.show()
